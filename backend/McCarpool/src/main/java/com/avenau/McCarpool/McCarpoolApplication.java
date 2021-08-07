55d751066f6ccfab71925615481505559819b747
aven au <aven.au@fdmgroup.com>
2021-08-07 22:06:03 +1000

    Init Commit

---
+++ backend/McCarpool/src/main/java/com/avenau/McCarpool/McCarpoolApplication.java
@@ -0,0 +1,13 @@
+package com.avenau.McCarpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McCarpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McCarpoolApplication.class, args);
+	}
+
+}
